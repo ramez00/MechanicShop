7abef6f5e774fac444610b305073458e465aeb3f
Ramez Abdalhamid <eng.ramez.mohamed@gmail.com>
2026-08-29 11:14:29 +0300

    Start in Infrastrure Layer - Add Behaviours in Application Layer - Add DependencyInjection Files

--- src/MechanicShop.Api/Program.cs
+++ src/MechanicShop.Api/Program.cs
@@ -1,4 +1,11 @@
+using MechanicShop.infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+        .AddApplicationLayer()
+        .AddInfrastructure(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
