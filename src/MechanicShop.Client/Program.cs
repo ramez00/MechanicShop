e5165c3bbec111a62c68d22698d3e796729da605
Ramez Abdalhamid <eng.ramez.mohamed@gmail.com>
2026-09-23 07:07:40 +0300

    Update Layout and Progrm File in Client Layer

--- src/MechanicShop.Client/Program.cs
+++ src/MechanicShop.Client/Program.cs
@@ -1,13 +1,34 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MechanicShop.Client;
+using Blazored.LocalStorage;
+
+using MechanicShop.Client.Hubs;
+using MechanicShop.Client.Identity;
 using MechanicShop.Client.Services;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+builder.Services.AddScoped(
+    sp => (IAccountManagement)sp.GetRequiredService<AuthenticationStateProvider>());
+
+builder.Services.AddTransient<BearerTokenHandler>();
+
+builder.Services.AddScoped<TimeZoneService>();
+
+builder.Services.AddScoped<WorkOrderHubClient>();
+
+builder.Services.AddHttpClient(
+    "MechanicShopClient",
+    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BearerTokenHandler>();
+
+builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddScoped<ServiceApi>();
 
 await builder.Build().RunAsync();
